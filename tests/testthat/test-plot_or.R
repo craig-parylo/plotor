bd69d3b3dd318c7b0f2d599fed95c76f2bd68a6d
Craig Parylo <craig.parylo2@nhs.net>
2025-02-21 11:27:03 +0000

    addresses #35 by adding to the test suite for code is expected to run fine and also for cases where we expect warnings, also added tests using snapshots and {vdiffr} to check chart outputs are consistent.

--- tests/testthat/test-plot_or.R
+++ tests/testthat/test-plot_or.R
@@ -1,38 +1,135 @@
 # main functions ---------------------------------------------------------------
-testthat::test_that("plot_or() does not produce messages or warnings", {
-  testthat::expect_silent({
 
-    # titanic lr model
+## test successful ----
+testthat::test_that("plot_or() does not produce messages or warnings", {
+
+  # titanic lr model
+  testthat::expect_silent({
     lr <- readRDS(file = testthat::test_path('test_data', 'lr_titanic.Rds'))
     plotor::plot_or(lr)
+  })
 
-    # diabetes lr model
+  # diabetes lr model
+  testthat::expect_silent({
     lr <- readRDS(file = testthat::test_path('test_data', 'lr_diabetes.Rds'))
     plotor::plot_or(lr)
+  })
 
-    # infertility lr model
+  # infertility lr model
+  testthat::expect_silent({
     lr <- readRDS(file = testthat::test_path('test_data', 'lr_infert.Rds'))
     plotor::plot_or(lr)
-
   })
+
 })
 
 testthat::test_that("table_or() does not produce messages or warnings", {
-  testthat::expect_silent({
 
-    # titanic lr model
+  # titanic lr model
+  testthat::expect_silent({
     lr <- readRDS(file = testthat::test_path('test_data', 'lr_titanic.Rds'))
     plotor::table_or(lr)
+  })
 
-    # diabetes lr model
+  # diabetes lr model
+  testthat::expect_silent({
     lr <- readRDS(file = testthat::test_path('test_data', 'lr_diabetes.Rds'))
     plotor::table_or(lr)
+  })
 
-    # infertility lr model
+  # infertility lr model
+  testthat::expect_silent({
     lr <- readRDS(file = testthat::test_path('test_data', 'lr_infert.Rds'))
     plotor::table_or(lr)
-
   })
+
+})
+
+## snapshots -------
+testthat::test_that("table_or() produces output equivalent to a snapshot", {
+
+  # titanic lr model
+  testthat::expect_snapshot({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_titanic.Rds'))
+    plotor::table_or(lr)
+  },
+  cran = FALSE)
+})
+
+testthat::test_that("plot_or() produces plots equivalent to a snapshot", {
+
+  # titanic lr model
+  vdiffr::expect_doppelganger({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_titanic.Rds'))
+    plotor::plot_or(lr)
+  },
+  title = "plot_titanic", cran = FALSE)
+
+  # diabetes lr model
+  vdiffr::expect_doppelganger({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_diabetes.Rds'))
+    plotor::plot_or(lr)
+  }, title = "plot_diabetes", cran = FALSE)
+
+  # infertility lr model
+  vdiffr::expect_doppelganger({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_infert.Rds'))
+    plotor::plot_or(lr)
+  }, title = "plot_infert", cran = FALSE)
+})
+
+testthat::test_that("table_or() produces {gt} tables equivalent to a snapshot", {
+
+  # titanic lr model
+  testthat::expect_snapshot({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_titanic.Rds'))
+    plotor::table_or(lr, output = 'gt')
+  }, cran = FALSE)
+
+  # diabetes lr model
+  testthat::expect_snapshot({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_diabetes.Rds'))
+    plotor::table_or(lr, output = 'gt')
+  }, cran = FALSE)
+
+  # infertility lr model
+  testthat::expect_snapshot({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_infert.Rds'))
+    plotor::table_or(lr, output = 'gt')
+  }, cran = FALSE)
+
+})
+
+## test failure -----
+
+testthat::test_that("table_or() and plot_or() handle issues gracefully", {
+
+  # not a binomial glm model
+  testthat::expect_error({
+    lr <- readRDS(file = testthat::test_path('test_data', 'nonlr_streptb.Rds'))
+    plotor::plot_or(lr)
+  })
+  testthat::expect_error({
+    lr <- readRDS(file = testthat::test_path('test_data', 'nonlr_streptb.Rds'))
+    plotor::table_or(lr)
+  })
+
+  # non-valid conf_level
+  testthat::expect_error({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_titanic.Rds'))
+    plotor::plot_or(lr, conf_level = "95")
+  })
+  testthat::expect_error({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_titanic.Rds'))
+    plotor::table_or(lr, conf_level = "95")
+  })
+
+  # non-valid output type requested
+  testthat::expect_error({
+    lr <- readRDS(file = testthat::test_path('test_data', 'lr_titanic.Rds'))
+    plotor::table_or(lr, output = "pink_elephant")
+  })
+
 })
 
 # validation functions ---------------------------------------------------------
@@ -49,10 +146,10 @@ testthat::test_that("validate_conf_level_input() works as expected", {
   testthat::expect_equal(plotor:::validate_conf_level_input(0.99), 0.99)
 
   # inputs outside expected range - parse to valid inputs
-  # testthat::expect_equal(plotor:::validate_conf_level_input(80), 0.80)
-  # testthat::expect_equal(plotor:::validate_conf_level_input(95), 0.95)
-  # testthat::expect_equal(plotor:::validate_conf_level_input(99), 0.99)
-  # testthat::expect_equal(plotor:::validate_conf_level_input(99.9), 0.999)
+  testthat::expect_equal(plotor:::validate_conf_level_input(80), 0.80)
+  testthat::expect_equal(plotor:::validate_conf_level_input(95), 0.95)
+  testthat::expect_equal(plotor:::validate_conf_level_input(99), 0.99)
+  testthat::expect_equal(plotor:::validate_conf_level_input(99.9), 0.999)
 
   # inputs outside expected range - expect messages informing of the change
   testthat::expect_message(plotor:::validate_conf_level_input(-1))
