ef298e1bb8440fedd7f6775b1a5fe4b20aa4c0e5
Craig Parylo <craig.parylo2@nhs.net>
2025-08-16 23:14:45 +0100

    fixes typo

--- R/plot_or.R
+++ R/plot_or.R
@@ -862,7 +862,7 @@ suggest_check_or <- function(
 #' This function provides a vectorised approach to data anonymisation that:
 #' - Suppresses counts below 10 by replacing them with the string "<10".
 #' - Rounds counts of 10 or more to the nearest multiple of 5.
-#' - Formats counts using `scales::label_number_auto()` to include thousands separators, which repects the locale settings for choice of the big mark.
+#' - Formats counts using `scales::label_number_auto()` to include thousands separators, which respects the locale settings for choice of the big mark.
 #'
 #' The function returns a vector of strings representing the anonymised counts.
 #'
